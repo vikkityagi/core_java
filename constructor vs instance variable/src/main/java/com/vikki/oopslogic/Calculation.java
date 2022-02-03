1b7154f65ea5b67f5877d80b321a3a0b3f4474cf
vikkityagi <vikkityagi1271998@gmail.com>
2022-02-03 15:07:28 +0530

    changes will be done

---
+++ constructor vs instance variable/src/main/java/com/vikki/oopslogic/Calculation.java
@@ -0,0 +1,10 @@
+package com.vikki.oopslogic;
+
+class Calculation {
+    String name="Sita";
+    int rollno=1000;
+    Calculation(String name,int rollno){
+        this.name=name;
+        this.rollno=rollno;
+    }
+}
