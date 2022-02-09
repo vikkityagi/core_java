dd8c0164d4469119ef5ff587823f2619a0461b6f
vikkityagi <vikkityagi1271998@gmail.com>
2022-02-09 09:00:20 +0530

    ok

---
+++ interface/src/main/java/com/vikki/hiding/Rectangleinterface.java
@@ -0,0 +1,14 @@
+package com.vikki.hiding;
+
+public interface Rectangleinterface {
+
+    void setSide(float l,float b);
+
+    float getSide1();
+
+    float getSide2();
+
+    float areaRectangle();
+
+    float perimeterRectangle();
+}
